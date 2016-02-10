85dcfb38af29bec8975a1d6c09e2e3f172944271
Sam Morrison <sorrison@gmail.com>
2016-02-10 15:57:35 +1100

    Support midonet type driver for ML2
    Midonet now works under ML2 and provides the midonet and uplink
    network type
    
    Change-Id: Id6948c98ffe5b0ff4a18ec44637480a6b68a00a6

---
+++ manifests/plugins/ml2/midonet.pp
@@ -0,0 +1,34 @@
+# == Class: neutron::plugins::ml2::midonet
+#
+# Configure the Mech Driver for midonet neutron plugin
+#
+# === Parameters:
+#
+# [*midonet_uri*]
+#   (required) MidoNet API server URI.
+#   Defaults to 'http://localhost:8080/midonet-api'
+#
+# [*username*]
+#   (required) MidoNet admin username.
+#
+# [*password*]
+#   (required) MidoNet admin password.
+#
+# [*project_id*]
+#   (required) Name of the project that MidoNet admin user belongs to.
+#
+class neutron::plugins::ml2::midonet (
+  $midonet_uri = 'http://localhost:8080/midonet-api',
+  $username    = 'admin',
+  $password    = false,
+  $project_id  = 'admin',
+) {
+
+  neutron_plugin_ml2 {
+    'MIDONET/midonet_uri' : value => $midonet_uri;
+    'MIDONET/username'    : value => $username;
+    'MIDONET/password'    : value => $password;
+    'MIDONET/project_id'  : value => $project_id;
+  }
+
+}
