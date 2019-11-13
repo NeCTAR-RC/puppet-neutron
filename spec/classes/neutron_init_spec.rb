c9adb9b4328422a0f2e506fdaf598fc7635c9dd1
Jake Yip <jake.yip@unimelb.edu.au>
2019-11-13 11:57:33 +1100

    Add executor_thread_pool_size
    Change I36324bae39e05dde57b962948f832c22599c13d2 added
    executor_thread_pool_size as a new param to oslo::messaging::default,
    but that option was not updated in many modules like this one. This
    means a user cannot set executor_thread_pool_size, as setting it
    anywhere else will throw a duplicate declaration error.
    
    This change update the oslo::messaging::default call accordingly, and
    adds an option to set it, similar to Change
    Id4a61c39ef6392a3d656952467c756be9e68de91
    
    Change-Id: I709f3bd0076a68ce84d5c0e23afbbfcd23dc83a5

--- spec/classes/neutron_init_spec.rb
+++ spec/classes/neutron_init_spec.rb
@@ -112,6 +112,7 @@ describe 'neutron' do
       is_expected.to contain_neutron_config('DEFAULT/control_exchange').with_value('neutron')
       is_expected.to contain_neutron_config('DEFAULT/state_path').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_neutron_config('oslo_concurrency/lock_path').with_value('$state_path/lock')
+      is_expected.to contain_neutron_config('DEFAULT/executor_thread_pool_size').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_neutron_config('DEFAULT/transport_url').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_neutron_config('DEFAULT/rpc_response_timeout').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_neutron_config('DEFAULT/vlan_transparent').with_value('<SERVICE DEFAULT>')
